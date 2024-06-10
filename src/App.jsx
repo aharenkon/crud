c137dd5925125babe7322355be39558d56581a2d
Andrei Harenkov <pevec1@yandex.ru>
2024-06-10 06:08:30 +0300

    update commit 10-06-2024

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   function loadData() {
     setTimeout(() => {
-      fetch("https://crudback.axareact.ru/notes")
+      fetch("https://https://crudback-g12g.onrender.com/notes")
         .then((response) => {
           return response.json();
         })
@@ -36,7 +36,7 @@ function App() {
   function saveData() {
     let data = JSON.stringify(update);
     console.log(data);
-    fetch("https://crudback.axareact.ru/notes", {
+    fetch("https://https://crudback-g12g.onrender.com/notes", {
       method: "POST",
       body: data,
       headers: {
@@ -56,7 +56,7 @@ function App() {
 
   const deleteNote = (id) => {
     console.log(id);
-    fetch("https://crudback.axareact.ru/notes/" + id, {
+    fetch("https://https://crudback-g12g.onrender.com/notes/" + id, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
